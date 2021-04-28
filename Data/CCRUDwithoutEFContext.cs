4a00a860132d9d21b93f88c4672ff3364f58c56c
MrDocument <nqnk.2112@gmail.com>
2021-04-28 09:58:18 +0700

    commit

---
+++ Data/CCRUDwithoutEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CCRUDwithoutEF.Models;
+
+namespace CCRUDwithoutEF.Data
+{
+    public class CCRUDwithoutEFContext : DbContext
+    {
+        public CCRUDwithoutEFContext (DbContextOptions<CCRUDwithoutEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CCRUDwithoutEF.Models.BookViewModel> BookViewModel { get; set; }
+    }
+}
